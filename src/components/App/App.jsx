9e21a16764e8db4f7fa78fd1a3515dbf2ddde30f
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-08 23:17:37 -0300

    created component checkbox

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Checkbox from '../../shared/CheckBox/Checkbox'
+import AppContainer from '../AppContainer/AppContainer'
 import AppHeader from '../AppHeader/AppHeader'
+
 import { Container, Wrapper } from './App.styles'
 
+
+
 function App(){
+  const [lettuce, setLettuce ] = useState()
+  const [rice, setRice ] = useState()
+
     return <Wrapper>
        <Container>
             <AppHeader/>
+            <AppContainer
+              left={<div>
+                produtos disponiveis:
+      
+                <Checkbox
+                  value={lettuce}
+                  title="Alface"
+                  onClick={() => setLettuce(!lettuce)}
+                />
+                <Checkbox
+                  value={rice}
+                  title="Arroz"
+                  onClick={() => setRice(!rice)}
+                />
+              </div>}
+              middle={<div>
+                sua lista de compras
+              </div>}
+              right={<div>
+                estatisticas
+              </div>}
+            />
+            
        </Container>
     </Wrapper>
 }
