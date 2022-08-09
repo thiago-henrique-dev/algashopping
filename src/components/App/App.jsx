afc70a852b0aef995be60760f0c27ca12dfe28e3
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-08 23:55:52 -0300

    create component lineChart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Checkbox from '../../shared/CheckBox/Checkbox'
+import LineChart from '../../shared/LineChart/LineChart'
 import AppContainer from '../AppContainer/AppContainer'
 import AppHeader from '../AppHeader/AppHeader'
 
@@ -10,13 +11,22 @@ import { Container, Wrapper } from './App.styles'
 function App(){
   const [lettuce, setLettuce ] = useState()
   const [rice, setRice ] = useState()
+  const [apple, setApple ] = useState()
+  const [healthy, setHealthy] = useState(20)
+
+    useEffect(() => {
+      setTimeout(function (){
+        setHealthy(100)
+      }, 5000)
+    }, [])
+
 
     return <Wrapper>
        <Container>
             <AppHeader/>
             <AppContainer
               left={<div>
-                produtos disponiveis:
+                Produtos disponiveis:
       
                 <Checkbox
                   value={lettuce}
@@ -28,12 +38,31 @@ function App(){
                   title="Arroz"
                   onClick={() => setRice(!rice)}
                 />
+                <Checkbox
+                  value={apple}
+                  title="Apple"
+                  onClick={() => setApple(!rice)}/>
               </div>}
               middle={<div>
-                sua lista de compras
+                Sua lista de compras
               </div>}
               right={<div>
-                estatisticas
+                Estatisticas
+                <LineChart
+                color="#09f"
+                title="Frutas"
+                percentage={healthy}
+                />
+                <LineChart
+                color="#004d62"
+                title="Verduras"
+                percentage={healthy}
+                />
+                <LineChart
+                color="#004d61"
+                title="Legumes"
+                percentage={healthy}
+                />
               </div>}
             />
             
